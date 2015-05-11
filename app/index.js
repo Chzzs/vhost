f66e3da30449b7ac993f020a9a5c357405a27917
Chazz <chzzs89@gmail.com>
2015-05-11 17:57:39 +0200

    no such host fixed

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 var bouncy = require('bouncy');
-var server = bouncy(function (request, res, bounce) {
-  if(request.headers.host = 'www.hameauderageade.com'){
-    console.log('bounce to 3000');
-    bounce(3000);
-  } else if (request.headers.host = 'www.123catelier.fr') {
-    bounce(3001);
+var server = bouncy(function (request, response, bounce) {
+	console.log(request.headers.host);
+  if(request.headers.host == 'www.hameauderageade.com'){
+	console.log("use express.static");
+  } else if (request.headers.host == 'www.123catelier.fr') {
+	bounce(3000);
   } else {
-    response.status(404).end('no such host');
+	response.statusCode = 404;
+	response.setHeader("Content-Type", "text/html");
+	response.end("No such host\n");
   }
 });
 server.listen(80);
