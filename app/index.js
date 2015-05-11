92536ddb16c058acb7ff49a994236984edfb298c
Chazz <chzzs89@gmail.com>
2015-05-11 18:48:56 +0200

    express added to serve hameauderageade

--- app/index.js
+++ app/index.js
@@ -1,14 +1,16 @@
+
+var express = require('express');
 var bouncy = require('bouncy');
 var server = bouncy(function (request, response, bounce) {
 	console.log(request.headers.host);
   if(request.headers.host == 'www.hameauderageade.com'){
-	console.log("use express.static");
+		bounce(3000);
   } else if (request.headers.host == 'www.123catelier.fr') {
-	bounce(3000);
+		bounce(3001);
   } else {
-	response.statusCode = 404;
-	response.setHeader("Content-Type", "text/html");
-	response.end("No such host\n");
+		response.statusCode = 404;
+		response.setHeader("Content-Type", "text/html");
+		response.end("No such host\n");
   }
 });
 server.listen(80);
