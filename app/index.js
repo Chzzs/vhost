c4c4088bf98fc7accb2a9631c351391370bbaf31
Chazz <chzzs89@gmail.com>
2015-10-08 16:50:52 +0200

    moved canul api to canul dev, subdomain are really fixed this time

--- app/index.js
+++ app/index.js
@@ -2,18 +2,19 @@
 var express = require('express');
 var bouncy = require('bouncy');
 var server = bouncy(function (request, response, bounce) {
-  if(request.headers.host == 'www.hameauderageade.com'){
+	var host = request.headers.host;
+	if( host == 'www.hameauderageade.com' || host == 'hameauderageade.com' ) {
 		bounce(3000);
-  } else if (request.headers.host == 'www.123catelier.fr') {
+	} else if ( host == 'www.123catelier.fr' || host == '123catelier.fr' ) {
 		bounce(3001);
-  } else if (request.headers.host == 'www.canul.fr'|| request.headers.host == 'canul.fr') {
+	} else if ( host == 'www.canul.fr' || host == 'canul.fr' ) {
 		bounce(3002);
-  } else if (request.headers.host == 'api.canul.fr') {
+  	} else if ( host == 'dev.canul.fr' ) {
 		bounce(3004);
-  } else {
+  	} else {
 		response.statusCode = 404;
 		response.setHeader("Content-Type", "text/html");
 		response.end("No such host\n");
-  }
+  	}
 });
 server.listen(80);
